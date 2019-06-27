03f3be96ca9e775afb09bc838a67109a0eb9fd4d
OllegM <aarc@live.ru>
2019-06-27 22:53:17 +0300

    lazy catalog loading

--- ReactApp/shopping-app/src/App.jsx
+++ ReactApp/shopping-app/src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import SiteCatalog from './Components/SiteCatalog/SiteCatalog';
+import React, { Suspense } from 'react';
+// import SiteCatalog from './Components/SiteCatalog/SiteCatalog';
 import Header from './Components/Header/Header';
 import './App.css';
+
+const SiteCatalog = React.lazy(() => import('./Components/SiteCatalog/SiteCatalog'));
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <SiteCatalog />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SiteCatalog />
+        </Suspense>
       </div>
     );
   }
